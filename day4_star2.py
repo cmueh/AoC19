97c49b5c5d040cd3bc7cf07df3d1e910fe5c4954
Christoph Mühleder <8479331+cmueh@users.noreply.github.com>
2019-12-04 17:22:26 +0100

    Add files via upload

---
+++ day4_star2.py
@@ -0,0 +1,22 @@
+def never_decrease(num):
+    s = str(num)
+    return list(s) == sorted(s)
+
+
+def two_adj(num):
+    d = {}
+    for i in "0123456789":
+        d[i] = 0
+
+    for i in str(num):
+        d[i] += 1
+
+    return 2 in d.values()
+
+
+sum = 0
+for i in range(372037, 905157+1):
+     if never_decrease(i) and two_adj(i):
+         sum += 1
+
+print(sum)
